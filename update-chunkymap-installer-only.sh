bd4943aaa65d214b417a6c13ce83c2ffcfbb2410
poikilos <7557867+poikilos@users.noreply.github.com>
2016-02-19 13:38:19 -0500

    working on installer

--- update-chunkymap-installer-only.sh
+++ update-chunkymap-installer-only.sh
@@ -4,9 +4,11 @@ CHUNKYMAP_INSTALLER_DIR=~/minetest-stuff/minetest-chunkymap
 cd ~/minetest-stuff
 rm master.zip
 wget https://github.com/expertmm/minetest-chunkymap/archive/master.zip
+rm -f minetest-chunkymap.zip
 mv master.zip minetest-chunkymap.zip
 unzip minetest-chunkymap.zip
 #mv minetest-chunkymap-master minetest-chunkymap
+rm -Rf "$CHUNKYMAP_INSTALLER_DIR"
 mv minetest-chunkymap-master "$CHUNKYMAP_INSTALLER_DIR"
 #cd minetest-chunkymap
 chmod +x "$CHUNKYMAP_INSTALLER_DIR/install-chunkymap-on-ubuntu.sh"
--- update-chunkymap-on-ubuntu-from-web.sh
+++ update-chunkymap-on-ubuntu-from-web.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 CHUNKYMAP_INSTALLER_DIR=~/minetest-stuff/minetest-chunkymap
+cd $CHUNKYMAP_INSTALLER_DIR
 ./update-chunkymap-installer-only.sh
 #./install-chunkymap-on-ubuntu.sh
-
-
-cp -f "$CHUNKYMAP_INSTALLER_DIR/minetestmapper-numpy.py" "$HOME/minetest/util/minetestmapper-numpy.py"
-mkdir "$CHUNKYMAP_DEST"
-cp -f "$CHUNKYMAP_INSTALLER_DIR/chunkymap-regen.py" "$CHUNKYMAP_DEST/"
-#chmod +x "$CHUNKYMAP_DEST/chunkymap-regen.py"
-cp -f "$CHUNKYMAP_INSTALLER_DIR/chunkymap-regen.sh" "$CHUNKYMAP_DEST/"
-chmod +x "$CHUNKYMAP_DEST/chunkymap-regen.sh"
-cp -f "$CHUNKYMAP_INSTALLER_DIR/chunkymap-cronjob" "$CHUNKYMAP_DEST/"
-chmod +x "$CHUNKYMAP_DEST/chunkymap-cronjob"
-cp -f "$CHUNKYMAP_INSTALLER_DIR/set-minutely-crontab-job.sh" "$CHUNKYMAP_DEST/"
-chmod +x "$CHUNKYMAP_DEST/set-minutely-crontab-job.sh"
-
-python replace-with-current-user.py
+chmod +x "$CHUNKYMAP_INSTALLER_DIR/install-chunkymap-on-ubuntu.sh"
+./install-chunkymap-on-ubuntu.sh
