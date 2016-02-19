5b16f4dde4f73eb82d16de562eba8fc0b9738788
poikilos <7557867+poikilos@users.noreply.github.com>
2016-02-19 13:51:06 -0500

    working on installer

--- install-chunkymap-on-ubuntu-from-web.sh
+++ install-chunkymap-on-ubuntu-from-web.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 CHUNKYMAP_INSTALLER_DIR=~/minetest-stuff/minetest-chunkymap
+chmod +x update-chunkymap-installer-only.sh
 ./update-chunkymap-installer-only.sh
 sh $CHUNKYMAP_INSTALLER_DIR/install-chunkymap-on-ubuntu.sh
