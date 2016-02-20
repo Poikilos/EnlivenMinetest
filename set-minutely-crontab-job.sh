381c3244c81036cb4926ef3cfe2123402060b6b6
poikilos <7557867+poikilos@users.noreply.github.com>
2016-02-20 15:25:38 -0500

    working on crontab job setter

--- set-minutely-crontab-job.sh
+++ set-minutely-crontab-job.sh
@@ -3,6 +3,7 @@
 # NOTE: this works only since user is a field on Ubuntu (on some GNU/Linux systems it is not, which is implied by omission at http://www.adminschoice.com/crontab-quick-reference)
 # Minute, Hour, Day of Month, Month (1 to 12), Day of Week
 # m h dom mon dow user command
+
 cd ~
 MT_CHUNKYMAP_CRON_TMP=mts_cron.tmp
 if [! -f "crontab.1st"];
