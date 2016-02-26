0507c93e8017f27a9445c10b6f8d9082435957b8
poikilos <7557867+poikilos@users.noreply.github.com>
2016-02-26 08:30:34 -0500

    remove some hard tabs

--- replace-with-current-user.py
+++ replace-with-current-user.py
@@ -19,26 +19,26 @@ if "\\" not in home_minetest_chunkymap_path:
     if os.path.isdir(home_minetest_chunkymap_path):
         for filename in filenames:
             file_path = os.path.join(home_minetest_chunkymap_path, filename)
-			if os.path.isfile(file_path):
-				temp_path = file_path+".tmp"
-				os.rename(file_path, temp_path)
-				if not os.path.isfile(file_path):
-					ins = open(temp_path, 'r')
-					outs = open(file_path, 'w')
-					line = True
-					while line:
-						line = ins.readline()
-						if line:
-							line = line.replace("/home/owner", home_path)
-							outs.write(line+"\n")
-					outs.close()
-					os.remove(temp_path)
-					ins.close()
-				else:
-					print "FAILED to rewrite the file '"+file_path+"' (to change minetest util path to '"+home_minetest_chunkymap_path+"')--perhaps it is in use. Make the file writeable then try running "+__FILE__+" again."
-					input("Press enter to continue...")
-			else:
-				print "SKIPPED "+filename+" since not installed (probably ok since deprecated files are still listed here)"
+            if os.path.isfile(file_path):
+                temp_path = file_path+".tmp"
+                os.rename(file_path, temp_path)
+                if not os.path.isfile(file_path):
+                    ins = open(temp_path, 'r')
+                    outs = open(file_path, 'w')
+                    line = True
+                    while line:
+                        line = ins.readline()
+                        if line:
+                            line = line.replace("/home/owner", home_path)
+                            outs.write(line+"\n")
+                    outs.close()
+                    os.remove(temp_path)
+                    ins.close()
+                else:
+                    print "FAILED to rewrite the file '"+file_path+"' (to change minetest util path to '"+home_minetest_chunkymap_path+"')--perhaps it is in use. Make the file writeable then try running "+__FILE__+" again."
+                    input("Press enter to continue...")
+            else:
+                print "SKIPPED "+filename+" since not installed (probably ok since deprecated files are still listed here)"
     else:
         print "FAILED to find '"+home_minetest_chunkymap_path+"'"
         print "Please install a compatible version of minetest-server package, run minetestserver once, then if you were running a chunkymap installer that called this py file, re-run that installer (otherwise re-run this script if you are sure that installer was successful)."
