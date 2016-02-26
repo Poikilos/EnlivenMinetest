b8254aa97b482ed5cc5976846677501039783bfd
poikilos <7557867+poikilos@users.noreply.github.com>
2016-02-26 17:05:45 -0500

    working on screen command syntax

--- chunkymap-regen-loop.sh
+++ chunkymap-regen-loop.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-echo type screen -r to reconnect with this screen
-flock -n /var/run/chunkymap-loop.lockfile -c "screen -t chunkymapregen python /home/owner/minetest/util/chunkymap-regen.py"
+echo type screen -r  to reconnect with this screen
+# -S names the socket (-t only sets the title)
+#FAILS: flock -n /var/run/chunkymap-loop.lockfile -c "screen -S chunkymapregen python /home/owner/minetest/util/chunkymap-regen.py"
+#FAILS: screen -S chunkymapregen flock -n /var/run/chunkymap-loop.lockfile -c python /home/owner/minetest/util/chunkymap-regen.py
+screen -S chunkymapregen python /home/owner/minetest/util/chunkymap-regen.py
