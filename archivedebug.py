cdc671776af1c1f5f6490639571668a5b35685c5
poikilos <7557867+poikilos@users.noreply.github.com>
2016-03-18 19:23:17 -0400

    add message NOT YET IMPLEMENTED
    since script does nothing yet

--- archivedebug.py
+++ archivedebug.py
@@ -1,13 +1,21 @@
 import os
 import datetime
-
 from minetestinfo import *
-#os_name = "*x"
-#debug_name = "debug.txt"
-#debug_path = "/usr/share/minetest/worlds"
-#world_name = None
-#if os.sep=="\\":
-#    os_name = "windows"
-
-#print("os detected: "+os_name)
-
+if os.path.isdir(minetestinfo.get_var("profile_minetest_path")):
+    print(__file__+"...")
+    debug_txt_path = os.path.join(minetestinfo.get_var("profile_minetest_path"), "debug.txt")
+    debug_archived_folder_name = "debug_archived"
+    debug_archived_folder_path = os.path.join(minetestinfo.get_var("profile_minetest_path"), debug_archived_folder_name)
+    if os.path.isfile(debug_txt_path):
+        print("NOT YET IMPLEMENTED")
+    else:
+        print("There is no '"+debug_txt_path+"'")
+        if os.sep=="\\":
+            print("  (maybe it was already archived after last server activity,")
+            print("  or you are not running minetestserver and this script")
+            print("  does not apply to you).")
+        else:
+            print("  (maybe it was already archived after last server activity).")
+else:
+    print("Missing folder '"+profile_minetest_path+"'.")
+    print("The folder should be your .minetest folder or other folder containing 'debug.txt' (at least '"+debug_archived_folder_name+"' will be there if "+__file__+" ran successfully before).")
