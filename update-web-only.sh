34fe8f71fe877f772de4d5e3ee64c5d4c8058a1c
poikilos <7557867+poikilos@users.noreply.github.com>
2016-03-22 23:06:56 -0400

    working on php updater

--- update-web-only.sh
+++ update-web-only.sh
@@ -16,3 +16,18 @@ fi
 wget https://github.com/expertmm/minetest-chunkymap/raw/master/web/chunkymap.php
 sudo mv -f chunkymap.php $DEST_WWW_PATH/
 
+DEST_FULLNAME=$DEST_WWW_PATH/chunkymap.php
+if [ -f "$DEST_FULLNAME" ]; then
+  echo "$DEST_FULLNAME saved successfully."
+else
+  echo "FAILED to save $DEST_FULLNAME."
+fi
+
+DEST_FULLNAME=$DEST_WWW_PATH/viewchunkymap.php
+if [ -f "$DEST_FULLNAME" ]; then
+  echo "$DEST_FULLNAME saved successfully."
+else
+  echo "FAILED to save $DEST_FULLNAME."
+fi
+
+
