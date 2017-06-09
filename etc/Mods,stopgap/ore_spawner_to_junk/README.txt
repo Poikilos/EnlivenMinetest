16c218d71067ad56f0d161b9eb2b127da318c87c
poikilos <7557867+poikilos@users.noreply.github.com>
2017-06-09 20:29:03 +0000

    working on ore spawner and other stopgap issues

---
+++ etc/Mods,stopgap/ore_spawner_to_junk/README.txt
@@ -0,0 +1,21 @@
+Minetest mod: orespawner_to_junk
+==========================================
+A full fork is planned with a minetest configuration option to disable spawners to prevent this issue in the first place
+(however, this mod is helpful when the ore generators already exist in the world).
+Ore generation (from spawners mod) is not good for multiplayer balance so there should be an option to turn it off.
+You must also remove the lines from spawners/spawners_ores.lua
+(see spawners-microfork for version with this done already)
+
+
+
+Code: LGPLv2.1, assets: CC BY-SA 3.0 Unported
+
+Mod dependencies: default, slimenodes
+
+Authors of source code
+----------------------
+expertmm (github.com/expertmm)
+
+Authors of media (textures)
+---------------------------
+(no media)
---
+++ etc/Mods,stopgap/ore_spawner_to_junk/init.lua
@@ -0,0 +1,16 @@
+-- Copyright 2017 expertmm (GNU GPL v2.1 license)
+
+-- minetest.register_alias(name, convert_to)
+-- "This means that if the engine finds nodes with the name name in the world the node with the name convert_to is used instead. "
+-- <https://dev.minetest.net/minetest.register_alias>
+minetest.register_alias("spawners:stone_with_gold_spawner", "slimenodes:slime_liquid_cyan")
+minetest.register_alias("spawners:stone_with_iron_spawner", "slimenodes:slime_liquid_cyan")
+minetest.register_alias("spawners:stone_with_copper_spawner", "slimenodes:slime_liquid_cyan")
+
+minetest.register_alias("spawners:stone_with_gold_spawner_active", "slimenodes:slime_liquid_cyan")
+minetest.register_alias("spawners:stone_with_iron_spawner_active", "slimenodes:slime_liquid_cyan")
+minetest.register_alias("spawners:stone_with_copper_spawner_active", "slimenodes:slime_liquid_cyan")
+
+minetest.register_alias("spawners:stone_with_gold_spawner_waiting", "slimenodes:slime_liquid_cyan")
+minetest.register_alias("spawners:stone_with_iron_spawner_waiting", "slimenodes:slime_liquid_cyan")
+minetest.register_alias("spawners:stone_with_copper_spawner_waiting", "slimenodes:slime_liquid_cyan")
