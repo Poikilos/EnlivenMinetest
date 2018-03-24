d2d85bdf0772ed9706a47ac49e1d079bc26e3d57
poikilos <7557867+poikilos@users.noreply.github.com>
2018-03-24 18:43:52 -0400

    create mod to transition from dynamic_liquid

---
+++ patches/mods-stopgap/dynamic_liquid_to_default/README.txt
@@ -0,0 +1,7 @@
+Minetest mod: dynamic_liquid_to_default
+==========================================
+This Minetest mod was created to repair worlds that have materials from dynamic_liquid but where the dynamic_liquid mod was removed. The dynamic_liquid:* nodes will be converted to the most closely matching default:* nodes.
+
+Authors of source code
+----------------------
+expertmm (github.com/expertmm)
---
+++ patches/mods-stopgap/dynamic_liquid_to_default/init.lua
@@ -0,0 +1,8 @@
+-- Copyright 2017 expertmm (GNU GPL v2.1 license)
+
+-- minetest.register_alias(name, convert_to)
+-- "This means that if the engine finds nodes with the name name in the world the node with the name convert_to is used instead. "
+-- <https://dev.minetest.net/minetest.register_alias>
+minetest.register_alias("dynamic_liquid:clay", "default:clay")
+minetest.register_alias("dynamic_liquid:spring", "default:water_source")
+
