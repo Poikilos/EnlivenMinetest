759e9820ce2feb3f47b3ee34a682e685cf9f7d27
poikilos <7557867+poikilos@users.noreply.github.com>
2018-04-12 16:33:59 -0400

    remove traces of unecessary sudo

--- etc/minetestserver-update-from-git.sh
+++ etc/minetestserver-update-from-git.sh
@@ -23,12 +23,12 @@ if [ -d "$HOME/Downloads/minetest" ]; then
   cmake . -DENABLE_GETTEXT=on -DENABLE_CURSES=on -DENABLE_FREETYPE=on -DENABLE_LEVELDB=on -DENABLE_CURL=on -DENABLE_GETTEXT=on -DENABLE_REDIS=on -DENABLE_POSTGRESQL=on -DRUN_IN_PLACE=off -DCMAKE_BUILD_TYPE=Release $build_what
 
   make -j$(nproc)
-  #cd games; sudo git pull; sudo make -j$(nproc)
+  #cd games; git pull; make -j$(nproc)
   #git config global user.email ""
   #git config global user.name "expertmm"
   #git pull https://github.com/minetest/minetest_game.git
-  #sudo git fetch
-  #sudo git checkout HEAD games/minetest_game
+  #git fetch
+  #git checkout HEAD games/minetest_game
   sudo make install
 
   MT_GAMES_DIR="/usr/local/share/minetest/games"
