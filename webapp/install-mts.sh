47a5847ac684ac2c744ad9cea6a2b965b392bbdc
poikilos <7557867+poikilos@users.noreply.github.com>
2019-03-17 21:42:48 -0400

    fix typos, use existing minetest

--- webapp/install-mts.sh
+++ webapp/install-mts.sh
@@ -7,21 +7,25 @@ if [ ! -d "$flag_dir" ]; then
 fi
 cd "$extracted_name"
 flag_file="minetest/bin/minetestserver"
-if [ -f "$flag_file" ]; then
-    rm -f "$flag_file"
-fi
-if [ -f "$flag_file" ]; then
-    echo "ERROR: Nothing done since can't remove old '$flag_file'"
-    exit 1
-fi
-if [ -f "mtcompile-program.pl" ]; then
-    # perl mtcompile-program.pl build >& program.log
-    echo "Compiling via perl..."
-    perl mtcompile-program.pl build --server >& program.log
+#if [ -f "$flag_file" ]; then
+    #rm -f "$flag_file"
+#fi
+#if [ -f "$flag_file" ]; then
+    #echo "ERROR: Nothing done since can't remove old '$flag_file'"
+    #exit 1
+#fi
+if [ -d minetest ]; then
+    echo "using existing minetest..."
 else
-    # NOTE: no pl in $extracted_name, assuming bash:
-    echo "Compiling via bash..."
-    bash -e mtcompile-program.sh build >& program.log
+    if [ -f "mtcompile-program.pl" ]; then
+        # perl mtcompile-program.pl build >& program.log
+        echo "Compiling via perl..."
+        perl mtcompile-program.pl build --server >& program.log
+    else
+        # NOTE: no pl in $extracted_name, assuming bash:
+        echo "Compiling via bash..."
+        bash -e mtcompile-program.sh build >& program.log
+    fi
 fi
 if [ ! -f "$flag_file" ]; then
     echo "ERROR: Build did not complete--missing '$flag_file'"
@@ -40,35 +44,35 @@ if [ ! -f "$flag_file" ]; then
     echo "ERROR: not complete--couldn't create '$flag_file'"
     exit 1
 fi
-flag_dir="$HOME/games/Bucket_Game"
+flag_dir="$HOME/minetest/games/Bucket_Game"
 if [ ! -d "$flag_dir" ]; then
     echo "ERROR: missing $flag_dir"
     exit 1
 fi
-if [ ! -d "$HOME/games/ENLIVEN" ]; then
-    cp -R "$flag_dir" "$HOME/games/ENLIVEN"
-    echo "name = ENLIVEN" > "$HOME/games/ENLIVEN/game.conf"
+if [ ! -d "$HOME/minetest/games/ENLIVEN" ]; then
+    cp -R "$flag_dir" "$HOME/minetest/games/ENLIVEN"
+    echo "name = ENLIVEN" > "$HOME/minetest/games/ENLIVEN/game.conf"
 else
     mod_name=coderbuild
     echo "updating $mod_name..."
-    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/games/ENLIVEN/mods"
+    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/minetest/games/ENLIVEN/mods"
     mod_name=codercore
     echo "updating $mod_name..."
-    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/games/ENLIVEN/mods"
+    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/minetest/games/ENLIVEN/mods"
     mod_name=coderedit
     echo "updating $mod_name..."
-    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/games/ENLIVEN/mods"
+    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/minetest/games/ENLIVEN/mods"
     mod_name=coderfood
     echo "updating $mod_name..."
-    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/games/ENLIVEN/mods"
+    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/minetest/games/ENLIVEN/mods"
     mod_name=codermobs
     echo "updating $mod_name..."
-    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/games/ENLIVEN/mods"
+    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/minetest/games/ENLIVEN/mods"
     mod_name=decorpack
     echo "updating $mod_name..."
-    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/games/ENLIVEN/mods"
+    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/minetest/games/ENLIVEN/mods"
     mod_name=mtmachines
     echo "updating $mod_name..."
-    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/games/ENLIVEN/mods"
-    # cp -f "$flag_dir/mods/LICENSE" "$HOME/games/ENLIVEN/mods/LICENSE"
+    rsync -rt "$flag_dir/mods/$mod_name" "$HOME/minetest/games/ENLIVEN/mods"
+    # cp -f "$flag_dir/mods/LICENSE" "$HOME/minetest/games/ENLIVEN/mods/LICENSE"
 fi
