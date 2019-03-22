d53000fbcaf3a8faaf42bdbbe4779497d44c8438
poikilos <7557867+poikilos@users.noreply.github.com>
2019-03-22 15:31:37 -0400

    add getting started page

--- webapp/server.js
+++ webapp/server.js
@@ -370,6 +370,11 @@ app.get('/modding', function(req, res, next) {
         msg: "",
     });
 });
+app.get('/getting-started', function(req, res, next) {
+    res.render('pages/getting-started', {
+        msg: "",
+    });
+});
 app.get('/skin-upload-form', function(req, res, next) {
     //var ending = "";
     //ending += '<a href="/">Back to Main Site</a><br/>' + "\n";
