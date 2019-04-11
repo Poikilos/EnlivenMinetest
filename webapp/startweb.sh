ea777e37b92dafdb56c1c870c1a4d84cf7dc49f1
poikilos <7557867+poikilos@users.noreply.github.com>
2019-04-11 16:57:44 -0400

    fix startweb.sh syntax

--- webapp/startweb.sh
+++ webapp/startweb.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-if [ -f "`command -v screen`"]; then
+if [ -f "`command -v screen`" ]; then
     screen -S EnlivenMinetest node server.js
 else
-    nohup node server.js &
+    nohup node server.js & | exit 1
+    echo "node server.js is running in background. terminate as follows:"
+    echo "  killall node"
 fi
