c7a570d5b06715bf0b10b5f2d243f5fd60ad5bee
poikilos <7557867+poikilos@users.noreply.github.com>
2019-04-11 18:18:25 -0400

    fix startweb.sh completely

--- webapp/startweb.sh
+++ webapp/startweb.sh
@@ -2,7 +2,7 @@
 if [ -f "`command -v screen`" ]; then
     screen -S EnlivenMinetest node server.js
 else
-    nohup node server.js & | exit 1
+    nohup node server.js &
     echo "node server.js is running in background. terminate as follows:"
     echo "  killall node"
 fi
