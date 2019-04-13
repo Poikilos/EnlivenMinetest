cc3fc57b4be67944101c98f8dc8dd9eb9481acfc
poikilos <7557867+poikilos@users.noreply.github.com>
2019-04-13 00:02:44 -0400

    working on elk_legacy

--- patches/mods-stopgap/elk_legacy/init.lua
+++ patches/mods-stopgap/elk_legacy/init.lua
@@ -1,5 +1,7 @@
 -- ../generatemod.py (EnlivenMinetest) generated
 -- the original version of this file.
-codermobs:alias_mob("codermobs:elk_male", "codermobs:elk")
+if minetest.get_modpath("codermobs") ~= nil
+    mobs:alias_mob("codermobs:elk_male", "codermobs:elk")
+end
 -- minetest.register_alias("codermobs:elk_male", "codermobs:elk")
 
