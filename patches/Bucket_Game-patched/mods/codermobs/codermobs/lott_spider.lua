9409acc437a4d2e60723d1209c78fe37790666c2
poikilos <7557867+poikilos@users.noreply.github.com>
2019-04-16 15:03:12 -0400

    improve particles and spider sounds

---
+++ patches/Bucket_Game-patched/mods/codermobs/codermobs/lott_spider.lua
@@ -0,0 +1,131 @@
+-- Giant Spider. Descended from LOTT version.
+
+-- ===================================================================
+
+local lcname     = "lott_spider"
+local ucname     = "LOTT Spider"
+local msname     = "codermobs_" .. lcname
+local obj_name   = "codermobs:" .. lcname
+
+-- ===================================================================
+
+mobs_param = {
+    lcname       = lcname       ,
+    ucname       = ucname       ,
+    obj_name     = obj_name     ,
+
+    aoc          =  1           ,
+    obr          =  1           ,
+    day_mode     = true         ,
+    min_light    =  0           ,
+    max_light    = 30           ,
+    min_height   = -31000       ,
+    max_height   =  31000       ,
+    spawn_chance =  12000       ,
+    spawn_type   = "npc"        ,
+
+    spawn_nodes  = {
+        "default:junglegrass"           ,
+        "default:jungleleaves"          ,
+        "default:jungletree"            ,
+    } ,
+
+    add_egg   = true                    ,
+    egg_image = "default_stone.png"     ,
+}
+
+-- ===================================================================
+
+codermobs.adjust_param()
+
+-- ===================================================================
+
+minetest.register_craftitem (obj_name .. "_meat", {
+    description = "Cooked Meat",
+    inventory_image = msname .. "_meat.png",
+    on_use = minetest.item_eat (4),
+})
+
+minetest.register_craftitem (obj_name .. "_meat_raw", {
+    description = "Raw Meat",
+    inventory_image = msname .. "_meat_raw.png",
+})
+
+minetest.register_craft ({
+    type = "cooking",
+    output = obj_name .. "_meat",
+    recipe = obj_name .. "_meat_raw",
+})
+
+mobs_param.core_param = {
+    type = mobs_param.spawn_type ,
+    passive = false,
+    attacks_monsters = true,
+    hp_min = 15,
+    hp_max = 30,
+    collisionbox = {-0.9, -0.01, -0.7, 0.7, 0.6, 0.7},
+    textures = { msname .. ".png"} ,
+    visual_size = {x=7,y=7},
+    visual = "mesh",
+    mesh = msname .. ".x",
+    makes_footstep_sound = true,
+    view_range = 15,
+    walk_velocity = 1,
+    run_velocity = 3,
+    armor = 200,
+    damage = 3,
+
+    drops = {
+        {name = "farming:string",
+        chance = 2,
+        min = 1,
+        max = 3,},
+        {name = obj_name .. "_meat_raw",
+        chance = 4,
+        min = 1,
+        max = 2,},
+
+    },
+
+    light_resistant = true,
+    drawtype = "front",
+    water_damage = 5,
+    lava_damage = 5,
+    light_damage = 0,
+    on_rightclick = nil,
+    attack_type = "dogfight",
+
+    animation = {
+        speed_normal = 15,
+        speed_run = 15,
+        stand_start = 1,
+        stand_end = 1,
+        walk_start = 20,
+        walk_end = 40,
+        run_start = 20,
+        run_end = 40,
+        punch_start = 50,
+        punch_end = 90,
+    },
+    jump = true,
+    step = 1,
+    blood_texture = msname .. "_blood.png",
+
+    sounds = {
+        war_cry = "codermobs_eerie",
+        damage = "codermobs_damage_giant_exoskeleton",
+        death   = "codermobs_death_giant_bug",
+        attack  = "codermobs_use_giant_exoskeleton",
+    },
+}
+
+-- ===================================================================
+
+codermobs.setup_mob()
+
+mobs:alias_mob ("mobs_monster:spider" , obj_name)
+
+codermobs.log_done()
+
+-- ===================================================================
+-- End of file.
---
+++ patches/prepatch.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+# such as meld /home/owner/minetest/games/ENLIVEN/ /home/owner/git/EnlivenMinetest/patches/Bucket_Game-patched/
+me=`basename "$0"`
+
+customDie() {
+    if [ -z "$1" ]; then echo "Unknown error."; fi
+    echo "$1"
+    exit 1
+}
+
+project0=Bucket_Game
+project1=Bucket_Game-basis
+project2=Bucket_Game-patched
+project0_path="$HOME/git/EnlivenMinetest/webapp/linux-minetest-kit/minetest/games/$project0"
+patches="$HOME/git/EnlivenMinetest/patches"
+project1_path="$patches/$project1"
+project2_path="$patches/$project2"
+if [ ! -d "$patches" ]; then
+    customDie "You are missing $patches so a patch basis and patched target cannot be created there."
+fi
+licenses="license.txt LICENSE LICENSE.txt oldcoder.txt LICENSE.md license.md"
+usage() {
+    echo "$me <file_path>"
+    echo "* will be copied to $project1 and $project2"
+    echo
+    echo "Example:"
+    echo "$me mods/coderfood/food_basic/init.lua"
+    echo
+    echo "* copies the file to $project1 and $project2)"
+    echo "* also copies $licenses and same in .. and ../.."
+    echo
+    echo
+}
+
+
+
+if [ -z "$1" ]; then
+    usage
+    exit 1
+fi
+
+what=$1
+
+
+
+file0_path="$project0_path/$what"
+file1_path="$project1_path/$what"
+file2_path="$project2_path/$what"
+
+echo "* getting parent of $file0_path..."
+dir0="$(dirname -- "$(realpath -- "$file0_path")")"
+# can't get realpath when directory doesn't exist yet (we make it):
+dir1="$(dirname -- "$file1_path")"
+dir2="$(dirname -- "$file2_path")"
+
+dir0_p="$(dirname -- "$(realpath -- "$dir0")")"
+dir1_p="$(dirname -- "$dir1")"
+dir2_p="$(dirname -- "$dir2")"
+
+dir0_pp="$(dirname -- "$(realpath -- "$dir0_p")")"
+dir1_pp="$(dirname -- "$dir1_p")"
+dir2_pp="$(dirname -- "$dir2_p")"
+
+#echo "* checking $dir0_pp..."
+#echo "* checking $dir1_pp..."
+#echo "* checking $dir2_pp..."
+
+if [ ! -d "$project0_path" ]; then
+    customDie "ERROR: You must have '$project0' installed as '$project0_path'"
+fi
+
+if [ ! -f "$file0_path" ]; then
+    customDie "ERROR: Missing '$file0_path')"
+fi
+
+if [ ! -d "$dir1" ]; then
+    mkdir -p "$dir1" || customDie "Cannot mkdir $dir1"
+fi
+
+if [ ! -d "$dir2" ]; then
+    mkdir -p "$dir2" || customDie "Cannot mkdir $dir2"
+fi
+
+# if file1 exists, overwriting is ok--update basis so diff will make patch correctly
+echo "* updating $file1_path"
+cp -f "$file0_path" "$file1_path" || customDie "Cannot cp '$file0_path' '$file1_path'"
+
+if [ -f "$file2_path" ]; then
+    customDie "Nothing done since '$file2_path' already exists."
+fi
+echo "* creating $file2_path"
+cp -f "$file0_path" "$file2_path" || customDie "Cannot cp '$file0_path' '$file2_path'"
+
+
+eval "arr=($licenses)"
+for license in "${arr[@]}"; do
+    lic0="$dir0/$license"
+    lic1="$dir1/$license"
+    lic2="$dir2/$license"
+    if [ -f "$lic0" ]; then
+        echo "* updating LICENSE '$lic1'..."
+        cp -f "$lic0" "$lic1" || customDie "Cannot cp -f '$lic0' '$lic1'"
+        if [ ! -f "$lic2" ]; then
+            echo "  - also for $project2..."
+            cp --no-clobber "$lic0" "$lic2" || customDie "Cannot cp -f '$lic0' '$lic2'"
+        fi
+    fi
+    lic0="$dir0_p/$license"
+    lic1="$dir1_p/$license"
+    lic2="$dir2_p/$license"
+    if [ -f "$lic0" ]; then
+        echo "* updating LICENSE '$lic1'..."
+        cp -f "$lic0" "$lic1" || customDie "Cannot cp -f '$lic0' '$lic1'"
+        if [ ! -f "$lic2" ]; then
+            echo "  - also for $project2..."
+            cp --no-clobber "$lic0" "$lic2" || customDie "Cannot cp -f '$lic0' '$lic2'"
+        fi
+    fi
+    lic0="$dir0_pp/$license"
+    lic1="$dir1_pp/$license"
+    lic2="$dir2_pp/$license"
+    if [ -f "$lic0" ]; then
+        echo "* updating '$lic1'..."
+        cp -f "$lic0" "$lic1" || customDie "Cannot cp -f '$lic0' '$lic1'"
+        if [ ! -f "$lic2" ]; then
+            echo "  - also for $project2..."
+            cp --no-clobber "$lic0" "$lic2" || customDie "Cannot cp -f '$lic0' '$lic2'"
+        fi
+    fi
+done
+
+echo "Done."
+echo
+echo
+
