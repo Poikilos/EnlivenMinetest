ddd2eb06b6f4a825de68eea541edc43cf035aec7
poikilos <7557867+poikilos@users.noreply.github.com>
2019-04-28 21:02:25 -0400

    add lua_api meld script

---
+++ patches/doc-meld.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+nohup meld /home/owner/Downloads/git/minetest/minetest/doc/lua_api.txt ~/git/EnlivenMinetest/webapp/linux-minetest-kit/minetest/doc/lua_api.txt ~/git/EnlivenMinetest/patches/deploy-patched/doc/lua_api.txt &
