e03229989ca917d5ed4d5bc2edccfaed8eed5740
poikilos <7557867+poikilos@users.noreply.github.com>
2019-07-02 12:45:09 -0400

    add white-egg-texture-realistic-190702-vs-190628

---
+++ Bucket_Game-branches/white-egg-texture-realistic-190702-vs-190628.sh
@@ -0,0 +1,16 @@
+
+branch=Bucket_Game-branches/white-egg-texture-realistic
+cp $branch/mods/codermobs/codermobs/textures/codermobs_chicken_egg.png $my_bucket_game/mods/codermobs/codermobs/textures/
+cp $branch/mods/codermobs/mobs/textures/mobs_chicken_egg.png $my_bucket_game/mods/codermobs/mobs/textures/
+cp $branch/mods/codermobs/mobs/textures/mobs_chicken_egg_overlay.png $my_bucket_game/mods/codermobs/mobs/textures/
+cp $branch/mods/codercore/codersea/textures/mobs_chicken_egg_overlay.png $my_bucket_game/mods/codercore/codersea/textures/
+cp $branch/mods/codercore/codersea/textures/mobs_chicken_egg.png $my_bucket_game/mods/codercore/codersea/textures/
+cp $branch/mods/codercore/tsm_pyramids/textures/tsm_pyramids_mummy_egg.png $my_bucket_game/mods/codercore/tsm_pyramids/textures/
+cp $branch/mods/codercore/tsm_pyramids/textures/tsm_pyramids_mummy_egg.png $my_bucket_game/mods/codercore/tsm_pyramids/textures/
+
+#Licenses:
+cp $branch/mods/codercore/codersea/LICENSE $my_bucket_game/mods/codercore/codersea/
+cp $branch/mods/codercore/tsm_pyramids/README.txt $my_bucket_game/mods/codercore/tsm_pyramids/
+cp $branch/mods/codercore/tsm_pyramids/oldcoder.txt $my_bucket_game/white-egg-texture-realistic/mods/codercore/tsm_pyramids/
+cp $branch/mods/codermobs/mobs/license.txt $my_bucket_game/mods/codermobs/mobs/
+cp $branch/mods/codermobs/codermobs/LICENSE $my_bucket_game/mods/codermobs/codermobs/
