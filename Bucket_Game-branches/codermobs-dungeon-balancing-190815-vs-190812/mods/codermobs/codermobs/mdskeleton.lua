773127aacbb3f7b3bffb0117f995d71cfd7c57ed
poikilos <7557867+poikilos@users.noreply.github.com>
2019-08-15 17:31:57 -0400

    dungeon balancing patch

---
+++ Bucket_Game-branches/codermobs-dungeon-balancing-190815-vs-190812/mods/codermobs/codermobs/mdskeleton.lua
@@ -0,0 +1,112 @@
+-- mdoege skeleton.
+
+-- Mesh by Morn76
+-- Animation by Pavel_S
+
+-- ===================================================================
+
+local lcname     = "mdskeleton"
+local ucname     = "Mdoege Skeleton"
+local msname     = "codermobs_" .. lcname
+local obj_name   = "codermobs:" .. lcname
+
+-- ===================================================================
+
+mobs_param = {
+    lcname       = lcname       ,
+    ucname       = ucname       ,
+    obj_name     = obj_name     ,
+
+    aoc          =     1        ,
+    day_mode     = false        ,
+    min_light    =     0        ,
+    max_light    =     3        ,
+    min_height   = -1000        ,
+    max_height   =   500        ,
+    spawn_chance =    36        ,
+    spawn_type   = "monster"    ,
+
+    spawn_nodes  = {
+        "default:cobble"           ,
+        "default:mossycobble"      ,
+        "default:desert_stone"     ,
+        "default:sandstonebrick"   ,
+        "ethereal:gray_dirt"       ,
+    } ,
+
+    add_egg   = true               ,
+    egg_image = "default_dirt.png" ,
+}
+
+-- ===================================================================
+
+codermobs.adjust_param()
+
+-- ===================================================================
+
+mobs_param.core_param = {
+    type = mobs_param.spawn_type    ,
+    makes_footstep_sound = true     ,
+
+    armor          = 100            ,
+    attack_animals = true           ,
+    attack_type    = "dogfight"     ,
+    damage         =   2            ,
+    floats         =   0            ,
+    hp_max         =  33            ,
+    hp_min         =   7            ,
+    stepheight     = 1.2            ,
+    jump_height    =   0            ,
+    jump           = false          ,
+    passive        = false          ,
+    reach          =   2            ,
+    run_velocity   =   3            ,
+    view_range     =  15            ,
+    walk_velocity  =   1            ,
+
+    fall_damage    =   0            ,
+    lava_damage    =   0            ,
+    light_damage   =   2            ,
+    water_damage   =   0            ,
+
+    collisionbox   = { -0.40, -0.01, -0.40, 0.40, 1.80, 0.40 } ,
+    drawtype       = "front"                                   ,
+    mesh           = msname .. ".x"                            ,
+    textures       = { msname .. "_mesh.png" }                 ,
+    visual         = "mesh"                                    ,
+
+    animation      = {
+        stand_start  =   0 ,
+        stand_end    =  23 ,
+        walk_start   =  24 ,
+        walk_end     =  49 ,
+        run_start    =  24 ,
+        run_end      =  49 ,
+        shoot_start  =  50 ,
+        shoot_end    =  82 ,
+        hurt_start   =  85 ,
+        hurt_end     = 115 ,
+        death_start  = 117 ,
+        death_end    = 145 ,
+        speed_normal =  30 ,
+        speed_run    =  60 ,
+    } ,
+
+    drops          = {
+        { name="default:apple", chance=2, min=1, max=3 } ,
+    } ,
+
+    sounds         = {
+        death  = msname .. "_death"  ,
+        hurt   = msname .. "_hurt"   ,
+        random = msname .. "_random" ,
+    } ,
+}
+
+-- ===================================================================
+
+codermobs.setup_mob()
+codermobs.log_done()
+
+-- ===================================================================
+-- End of file.
---
+++ Bucket_Game-branches/codermobs-dungeon-balancing-190815-vs-190812/mods/codermobs/codermobs/oerkki.lua
@@ -0,0 +1,124 @@
+-- Oerkki.
+
+-- ===================================================================
+
+local lcname     = "oerkki"
+local ucname     = "Oerkki"
+local msname     = "codermobs_" .. lcname
+local obj_name   = "codermobs:" .. lcname
+
+-- ===================================================================
+
+mobs_param = {
+    lcname       = lcname       ,
+    ucname       = ucname       ,
+    obj_name     = obj_name     ,
+
+    aoc          =      1       ,
+    day_mode     =  false       ,
+    min_light    =      0       ,
+    max_light    =      7       ,
+    min_height   = -31000       ,
+    max_height   =    -51       ,
+    spawn_chance =    288       ,
+    spawn_type   = "monster"    ,
+
+    spawn_nodes  = {
+        -- "default:stone"         ,
+        "default:cobble"        ,
+        "default:desert_stone"  ,
+        "default:mossycobble"   ,
+        "default:sandstonebrick",
+    } ,
+
+    add_egg   = true                    ,
+    egg_image = "default_obsidian.png"  ,
+}
+
+-- ===================================================================
+
+codermobs.adjust_param()
+
+-- ===================================================================
+
+mobs_param.core_param = {
+    type = mobs_param.spawn_type ,
+
+    armor          = 100         ,
+    attack_type    = "dogfight"  ,
+    damage         =   4         ,
+    attack_animals = true           ,
+    fear_height    =   4         ,
+    hp_max         =  34         ,
+    hp_min         =   8         ,
+    stepheight     = 1.2         ,
+    jump_height    =   0         ,
+    jump           = false       ,
+    passive        = false       ,
+    pathfinding    = true        ,
+    reach          =   2         ,
+    run_velocity   =   3         ,
+    view_range     =  10         ,
+    walk_velocity  =   1         ,
+    floats         =   0         ,
+    runaway        = true        ,
+
+    lava_damage    =   4         ,
+    light_damage   =   1         ,
+    water_damage   =   2         ,
+
+    -- This mob eats torches
+    replace_offset =  -1                    ,
+    replace_rate   =   5                    ,
+    replace_what   = { "default:torch" }    ,
+    replace_with   = "air"                  ,
+
+    makes_footstep_sound = false ,
+
+    drops = {
+        { name = "default:obsidian" ,
+          chance = 3, min = 1, max = 2 } ,
+    },
+
+    immune_to = {
+        { "default:sword_wood" ,   0 } , -- No damage
+        { "default:gold_lump"  , -10 } , -- Heals by 10 points
+    },
+
+    sounds = {
+        random = "codermobs_oerkki" ,
+    },
+
+    animation = {
+        stand_start  =  0 ,
+        stand_end    = 23 ,
+        walk_start   = 24 ,
+        walk_end     = 36 ,
+        run_start    = 37 ,
+        run_end      = 49 ,
+        punch_start  = 37 ,
+        punch_end    = 49 ,
+        speed_normal = 15 ,
+        speed_run    = 15 ,
+    },
+
+    textures = {
+        { "codermobs_oerkki1.png" } ,
+        { "codermobs_oerkki2.png" } ,
+    },
+
+    collisionbox = { -0.4, -1, -0.4, 0.4, 0.9, 0.4 }    ,
+    mesh         = "codermobs_oerkki.b3d"               ,
+    visual       = "mesh"                               ,
+}
+
+-- ===================================================================
+
+codermobs.setup_mob()
+
+mobs:alias_mob ("mobs_monster:oerkki" , obj_name)
+
+codermobs.log_done()
+
+-- ===================================================================
+-- End of file.
