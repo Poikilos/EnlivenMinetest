1ff9250e1a9dbf0209f3ecb4891776cecbf3602c
poikilos <7557867+poikilos@users.noreply.github.com>
2019-09-27 13:32:55 -0400

    make one-line install into mult-line for clarity

--- utilities/deprecated/minetestserver-install-git-ENABLE_REDIS.sh
+++ utilities/deprecated/minetestserver-install-git-ENABLE_REDIS.sh
@@ -11,9 +11,15 @@ sudo apt-get update
 sudo apt-get install redis-server
 
 
-#if you skip the above, the below says missing: GetText, Curses, ncurses, Redis, SpatialIndex, Doxygen
+#if you skip the above, the below (from "one-line install") says missing: GetText, Curses, ncurses, Redis, SpatialIndex, Doxygen
 #cd "$HOME"  #done below by parameterless "cd" command
-sudo apt-get install -y git build-essential libirrlicht-dev libgettextpo0 libfreetype6-dev cmake libbz2-dev libpng12-dev libjpeg8-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-openssl-dev libluajit-5.1-dev liblua5.1-0-dev libleveldb-dev; cd; git clone https://github.com/minetest/minetest.git; cd minetest/games; git clone https://github.com/minetest/minetest_game.git; cd ..; cmake . -DENABLE_GETTEXT=1 -DENABLE_FREETYPE=1 -DENABLE_LEVELDB=1 -DENABLE_REDIS=1 -DBUILD_SERVER=TRUE -DBUILD_CLIENT=FALSE; make -j$(nproc); sudo make install;
+sudo apt-get install -y git build-essential libirrlicht-dev libgettextpo0 libfreetype6-dev cmake libbz2-dev libpng12-dev libjpeg8-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-openssl-dev libluajit-5.1-dev liblua5.1-0-dev libleveldb-dev
+cd
+git clone https://github.com/minetest/minetest.git
+cd minetest/games
+git clone https://github.com/minetest/minetest_game.git
+cd ..
+cmake . -DENABLE_GETTEXT=1 -DENABLE_FREETYPE=1 -DENABLE_LEVELDB=1 -DENABLE_REDIS=1 -DBUILD_SERVER=TRUE -DBUILD_CLIENT=FALSE; make -j$(nproc); sudo make install;
 # minetest;
 echo -e "\n\n\e[1;33mYou can run Minetest Server by typing \"minetestserver\" in a terminal.\e[0m"
 echo "Remember to add requirepass to /etc/redis/redis.conf -- a long password since redis is fast and can be bruteforced quickly"
