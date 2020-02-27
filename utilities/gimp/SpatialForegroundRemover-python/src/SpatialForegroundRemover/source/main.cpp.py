9d9e45eb72c6d9f1913e5db3354b400ba833a3e9
poikilos <7557867+poikilos@users.noreply.github.com>
2020-02-26 20:57:17 -0500

    Delete SFR for migrating it to to channel_tools.

--- utilities/gimp/SpatialForegroundRemover-python/src/SpatialForegroundRemover/source/main.cpp.py
+++
@@ -1,7 +0,0 @@
-#include <vd2/VDXFrame/VideoFilterEntry.h>
-
-VDX_DECLARE_VIDEOFILTERS_BEGIN()
-VDX_DECLARE_VIDEOFILTER(filterDef_SpatialForegroundRemover)
-VDX_DECLARE_VIDEOFILTERS_END()
-
-VDX_DECLARE_VFMODULE()
