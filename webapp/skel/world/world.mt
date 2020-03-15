f4206942dac0a33e89ee1ac7cc5e01e28c58d4f0
poikilos <7557867+poikilos@users.noreply.github.com>
2020-03-15 07:31:46 -0400

    removed files now in node-enliven-minetest repo

--- webapp/skel/world/world.mt
+++
@@ -1,5 +0,0 @@
-creative_mode = false
-player_backend = sqlite3
-gameid = Bucket_Game
-enable_damage = true
-backend = sqlite3
--- webapp/startweb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -f "`command -v screen`" ]; then
-    screen -S EnlivenMinetest node server.js
-else
-    nohup node server.js &
-    echo "node server.js is running in background. terminate as follows:"
-    echo "  killall node"
-fi
