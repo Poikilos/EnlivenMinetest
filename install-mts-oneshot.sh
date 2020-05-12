a12b7609ca2256ddad3019b641ce264ab487d8e4
poikilos <7557867+poikilos@users.noreply.github.com>
2020-05-12 10:56:20 -0400

    Add a one-shot compile and install script that can be piped to curl from the web.

---
+++ install-mts-oneshot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p ~/Downloads \
+  && cd ~/Downloads \
+  && wget -O reset-minetest-install-source.sh https://raw.githubusercontent.com/poikilos/EnlivenMinetest/master/reset-minetest-install-source.sh \
+  && chmod +x reset-minetest-install-source.sh \
+  && wget -O install-mts https://raw.githubusercontent.com/poikilos/EnlivenMinetest/master/install-mts.sh \
+  && chmod +x install-mts.sh \
+  && ./reset-minetest-install-source.sh
+  && ./install-mts.sh
