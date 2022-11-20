0a0fe09e6f78c095a7ab4c0edc94e0aa0a4dc8e5
poikilos <7557867+poikilos@users.noreply.github.com>
2022-11-20 18:57:00 -0500

    Add lmk scripts.

---
+++ utilities/fix-lmk-permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /opt
+sudo chown -R minebest:minebest linux-minetest-kit
+sudo chmod -R g+w linux-minetest-kit
---
+++ utilities/get-lmk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ -d "linux-minetest-kit" ]; then
+    >&2 echo "Error: linux-minetest-kit is already here. To update it safely, rename it, otherwise do:"
+    >&2 echo "    rm -rf '`pwd`/linux-minetest-kit'"
+    exit 1
+fi
+cd ~
+ZIP_NAME=linux-minetest-kit.zip
+REMOTE_ZIP=mtio:/opt/minebest/assemble/prod/linux-minetest-kit.zip
+rsync -tvP $REMOTE_ZIP .
+code=$?
+if [ $code -ne 0 ]; then
+    >&2 echo "Error: 'rsync -tvP $REMOTE_ZIP .' failed in \"`pwd`\". Make sure it was built on the other server or change $0 to match the correct location."
+    exit $code
+fi
+unzip $ZIP_NAME
+code=$?
+if [ $code -ne 0 ]; then
+    >&2 echo "Error: 'unzip $ZIP_NAME' failed in \"`pwd`\"."
+    exit $code
+fi
+exit 0
